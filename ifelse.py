afba58e719bad0d1e5c83cd36f7e5cc41b6d70d8
Pujarani06052001 <103930803+Pujarani06052001@users.noreply.github.com>
2022-04-26 14:56:38 +0530

    Add files via upload

---
+++ ifelse.py
@@ -0,0 +1,482 @@
+# age=int(input("enter a number"))
+# if age>=18:
+#     print("elgible for voting",age)
+# else:
+#     print("not elible for voting",age)
+
+
+# a=tnt(input("enter a number"))
+# if a%2==0:
+#     print("even",a)
+# else:
+#     print("odd",a)
+
+
+# a=int(input(enter a number"))
+# if a%7==:
+#     print("divisible by 7",a)
+# else:
+#     print("not divisiblby 7",a)
+
+
+# a=int(input("enter a number "))
+# if a%5==0:
+#     print("hello")
+# else:
+#     print("bye")
+
+
+
+# num=int(input("enter a number"))
+# if num%10==0:
+#a=num%2==0:
+#     print("last dight of number divisible by 2")
+# else:
+#     ("last dight of number is not divisible by 2")
+
+
+
+# grade= int(input("Enter marks"))
+# if  grade> 90:
+#     print("Grade is A")
+# elif grade > 80 and pe <=90:
+#     print("Grade is B")
+# elif grade >=60 and per <= 80:
+#     print("Grade is C")
+# elif grade < 60:
+#     print("Grade is D")
+
+
+
+# num=int(input("enter a number"))
+# if num%400==0:
+#     print("leap year")
+# else:
+#  	print("not leap year")
+
+
+# a=int(input("Enter any number"))
+# if a==1:
+#     print("Sunday")
+# elif a==2:
+#     print("Monday")
+# elif a==3:
+#     print("Tuesday")
+# elif a==4:
+#    print("Wednesday")
+# elif a==5:
+#    print("Thursday")
+# elif a==6:
+#    print("Friday")
+# elif a==2:
+#    print("Saturday")
+# else:
+#  print("nothing")
+
+
+
+# num=int(input("Enter any number"))
+# a=num%10
+# if a%3==0:
+#      print("Last digit of number is divisible by 3 ")
+# else:
+#      print("Last digit of number is not divisible by 3 ")
+
+
+# a=int(input("Enter any number: "))
+# if a==1a:
+#     print("January")
+# elif a==2:
+#     print("February")
+# elif a==3:
+#     print("March")
+# elif a==4:
+#     print("April")
+# elif a==5:
+#     print("May")
+# elif a==6:
+#     print("June")
+# elif a==7:
+#     print("July")
+# elif a==8:
+#     print("August")
+# elif a==9:
+#     print("September")
+# elif a==10:
+#     print("October")
+# elif a==11:
+#     print("November")
+# elif a==12:
+#     print("December")
+# else:
+#     print("nothing")
+
+
+
+
+ 
+#a=int(input("Enter any number: "))
+# if a==1a:
+#     print("January")
+# elif a==2:
+#     print("February")
+# elif a==3:
+#     print("March")
+# elif a==4:
+#     print("April")
+# elif a==5:
+#     print("May")
+# elif a==6:
+#     print("June")
+# elif a==7:
+#     print("July")
+# elif a==8:
+#     print("August")
+# elif a==9:
+#     print("September")
+# elif a==10:
+#     print("October")
+# elif a==11:
+#     print("November")
+# elif a==12:
+#     print("December")
+# else:
+#     print("nothing")
+
+
+
+#a=int(input("Enter any number: "))
+# if a==1a:
+#     print("January")
+# elif a==2:
+#     print("February")
+# elif a==3:
+#     print("March")
+# elif a==4:
+#     print("April")
+# elif a==5:
+#     print("May")
+# elif a==6:
+#     print("June")
+# elif a==7:
+#     print("July")
+# elif a==8:
+#     print("August")
+# elif a==9:
+#     print("September")
+# elif a==10:
+#     print("October")
+# elif a==11:
+#     print("November")
+# elif a==12:
+#     print("December")
+# else:
+#     print("nothing")
+
+
+
+
+# city=input("enter a city name::")
+# if city=="Delhi":
+# 	print("Red fort")
+# elif city=="agra":
+# 	print("taj mahal")
+# elif city=="jaipur":
+# 	print("jal mahal")
+# else:
+# 	print("nothing")
+
+
+
+# if (a > 5 and a <=10):    
+#          print("Hello")    
+#     else:    
+#         print("Bye")
+
+
+
+# age=int(input("Enter your age"))
+# if age>=18:
+#     print("Eligible for Voting")
+# else:
+#     print("Not eligible for voting")
+
+
+# age=int(input("Enter your age::"))
+# if age >=60:
+
+#     print("Senior Citizen")
+# else:
+#     print("Not a Senior Citizen")
+
+
+# num1 = int(input("Enter first number"))
+# num2 = int(input("Enter second number"))
+# if num1 > num2:
+#      print("lowest number is :", num2)
+# else:
+#      print("largest number is :", num1)
+
+
+# a= int(input("Enter first number"))
+# b= int(input("Enter second number"))
+# if a > b:
+#      print("smaller number is :", b)
+# else:
+#      print("smaller number is :", a)
+
+
+
+# a= int(input("Enter first number"))
+# b = int(input("Enter second number"))
+# if a>b:
+#      print("largest number is :", a)
+# else:
+#      print("largest number is :", b)
+
+
+
+# num = int(input("Enter first number"))
+# if num > 0 :
+#     print("Number is positive")
+# else:
+#     print("Number is negative")
+
+
+
+# a= int(input("Enter first number"))
+# if a%2==0:
+#     print("Number is even")
+# else:
+#     print("Number is odd")
+
+
+# num1 = int(input("Enter first number"))
+# if num1%2==0 and num1%3==0:
+#       print("divisible by 2 and 3 both")
+# else:
+#       print(" not divisible by both")
+
+
+
+# a = int(input("Enter first number"))
+# b = int(input("Enter second number"))
+# c = int(input("Enter third number"))
+# if a > b and a > c:
+#       print("Greatest number is ", a)
+# if b > a and b> c:
+#       print("Greatest number is ", b)
+# if c > b and c > a:
+#      print("Greatest number is ", c)
+
+
+# temp = int(input("Enter temperature of water"))
+# if temp >=100:
+#     print("Water is boiling")
+# else:
+#     print("Water is not boiling")
+
+
+
+# ch=input("Enter any character")
+# vow="aeiouAEIOU"
+# if ch in vow:
+#      print("Entered character is vowel")
+# else:
+#      print("Entered character is not vowel")
+
+
+
+# per= int(input("Enter total number of working days"))
+# if per <75 :
+#      print("You are not eligible for exams")
+# else:
+#      print("You are eligible for writing exam")
+
+
+
+# a = int(input("Enter total number of working days"))
+# b= int(input("Enter number of days  absent"))
+# per=(a-b)/a*100
+# print("Your attendance is ",per)
+# if per <75 :
+#      print("You are not eligible for exams")
+# else:
+#      print("You are eligible for writing exam")
+
+
+
+
+
+# per = int(input("Enter percentage"))
+# if per> 80:
+#      print("Grade is A+")
+# elif per >60 and per <=80:
+#      print("Grade is A")
+# elif per > 50 and per <=60:
+#      print("Grade is B+")
+# elif per > 45 and per <=50:
+#      print("Grade is B")
+# elif per > 25 and per <=45:
+#      print("Grade is C")
+# elif per <25:
+#     print("Grade is D")
+
+
+# ser=int(input("Enter the time period of service"))
+# sal =int(input("Enter your salary"))
+# if ser > 10:
+#      b=10/100*sal
+# if ser >=6 and ser <=10:
+#      b = 8/100*sal
+# if ser < 6:
+#     b = 5/100*sal
+# print("Bonus is ", b)
+
+
+
+
+# a=int(input("enter a number"))
+# b=int(input("enter a number"))
+# c=input("enter a oprater")
+# if c=="+":
+#     print(a+b)
+# elif c=="-":
+#     print(a-b)
+# elif c=="%":
+#     print(a%b)
+# elif c=="*":
+#     print(a*b)
+# elif c=="**":
+#     print(a**b)
+
+
+# a=int(input("Enter first number"))
+
+# b=int(input("Enter second number"))
+
+# c=int(input("Enter third number"))
+
+# if (a > b and a < c) or (a < b and a > c):
+
+#      print("Middle number is " , a)
+
+# if (b > a and b < c) or (b < a and b > c):
+
+#      print("Middle number is" , b)
+
+# if (c > b and c < a) or (c < b and c > a):
+
+#      print("Middle number is" , c)
+
+
+
+
+# a=int(input('enter a age::'))
+# b=int(input('enter a age::'))
+# if a>b:
+# 	print('oldest')
+# elif a<b:
+# 	print('youngest')
+# else:
+# 	print('nothing')
+
+
+# a=int(input('enter a number::'))
+# b=int(input('enter a number::'))
+# c=int(input('enter a number::'))
+# d=int(input('enter a number::'))
+# if a>b:
+# 	print('greater than a')
+# elif c>d:
+# 	print('greater than c')
+# else:
+# 	print('nothing')
+
+
+# strong passward
+
+# n=input('enter a number::')
+# if n> "A" and n<"Z" or n>"a" and n<"z" and n>"0" and n<"9"or n>"@" and n<"#":
+# 	print("strong passward")
+# else:
+# 	print("wrong passward")
+
+
+# sale=int(input("enter a shal:"))
+# cost=int(input("enter a cost::"))
+# if sale==cost:
+# 	print("no frofit and no loss")
+# elif sale >cost:
+# 	print("profit",sale-cost)
+# else:
+# 	print("loss",cost-sale)
+
+
+# a=int(input("enter the value"))
+# if a%3==0 and a%7==0:
+# 	print("Navegurukul")
+# elif a%3==0:
+# 	print("nave")
+# elif a%7==0:
+# 	print("gurukul")
+# else:
+# 	print("not")
+
+
+# a=int(input('enter a number::'))
+# if a%5==0:
+# 	print('hello')
+# else:
+# 	print('bye')
+# if a%6==0:
+# 	print("good bye ")
+
+# a=[4,[7,[3,4,7],5],6]
+# i=0
+# while i<len(a):
+#     if a[i]==a[i]:
+#         print(a[i])
+#         break
+#     i+=1  
+
+
+
+
+# a=int(input("enter a number"))
+# b=int(input("enter a number"))
+# c=input("enter a oprater")
+# if c=="+":
+#     print(a+b)
+# elif c=="-":
+#     print(a-b)
+# elif c=="%":
+#     print(a%b)
+# elif c=="*":
+#     print(a*b)
+# elif c=="**":
+#     print(a**b)
+# else:
+#     print("nothiung")
+
+
+
+
+# age=int(input('enter a number:'))
+# if age>=18:
+# 	print('elgibal or voting')
+# elif age <=18:
+# 	print('not eligibal for voting')
+# else:
+# 	print('nothig')
+
+
+# a=int(input("enter the value"))
+# if a%3==0 and a%7==0:
+# 	print("Navegurukul")
+# elif a%3==0:
+# 	print("nave")
+# elif a%7==0:
+# 	print("gurukul")
+# else:
+# 	print("not")
